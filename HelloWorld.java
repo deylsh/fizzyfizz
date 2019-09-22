767152e618af3cd28f4c3cf24c38c26dab6562b6
Luis <clpoehl@gmail.com>
2019-09-22 20:28:28 +0200

    "Universe"

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
+
+
 public class HelloWorld {
-  public static void main(String[] args) {
-    System.out.println("Hello SwEng");
+public static int computeUltimateAnswer() {
+        return 6*7;
+}
+public static void main(String[] args) {
+	System.out.println("Hello SwEng");
+	System.out.println("The answer to the ultimate question of Life, The Universe, and Everything is: " + computeUltimateAnswer());
   }
 }
